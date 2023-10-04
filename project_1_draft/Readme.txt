6740c258f0475b9decccf653330ad1e09c1f3888
fryclan <fry.jeffery123454@gmail.com>
2023-10-04 10:06:39 -0700

    added dunnder string to adventurer class in player_character.py to help with file io.

---
+++ project_1_draft/Readme.txt
@@ -0,0 +1,4 @@
+You will need the spells.py, player_character.py, and the adventure_class.py files it will created a txt document that has the output
+
+to Run:
+run the player_character.py in vscode
---
+++ project_1_draft/Spells.py
@@ -0,0 +1,25 @@
+"""Lists of spells for different classes"""
+
+wizard_cantrips = ["Acid Splash", "Blade Ward", "Booming Blade", "Chill Touch", 
+                    "Control Flames", "Create Bonfire", "Dancing Lights", "Encode Thoughts", 
+                    "Fire Bolt", "Friends", "Frostbite", "Green Flame Blade", "Gust", 
+                    "Infestation", "Light", "Lightning Lure", "Mage Hand", "Mending", 
+                    "Message", "Mind Sliver", "Minor Illusion", "Mold Earth", 
+                    "Poison Spray", "Prestidigitation", "Ray Of Frost", "Sapping Sting", 
+                    "Shape Water", "Shocking Grasp", "Sword Burst", "Thunderclap", 
+                    "Toll The Dead", "True Strike"]
+
+wizard_level1 = ["Absorb Elements", "Alarm", "Burning Hands", "Catapult", "Cause Fear", "Charm Person", 
+                "Chromatic Orb", "Color Spray", "Comprehend Languages", "Detect Magic", "Disguise Self", 
+                "Distort Value", "Earth Tremor", "Expeditious Retreat", "False Life", "Feather Fall", 
+                "Find Familiar", "Fog Cloud", "Frost Fingers", "Gift Of Alacrity", "Grease", 
+                "Ice Knife", "Identify", "Illusory Script", "Jump", "Longstrider", "Mage Armor", 
+                "Magic Missile", "Magnify Gravity", "Protection From Evil And Good", "Ray Of Sickness", 
+                "Shield", "Silent Image", "Silvery Barbs", "Sleep", "Snare", "Thunderwave", "Witch Bolt"]
+
+cleric_cantrips = ["Guidance", "Light", "Mending", "Resistance", "Sacred Flame", "Spare The Dying", 
+                   "Thaumaturgy", "Toll The Dead", "Word Of Radiance"]
+
+cleric_level1 = ["Bane", "Bless", "Ceremony", "Command", "Cure Wounds", "Detect Evil And Good", 
+                 "Detect Magic", "Detect Poison And Disease", "Guiding Bolt", "Healing Word", "Inflict Wounds", 
+                 "Protection From Evil And Good", "Purify Food And Drink", "Sanctuary", "Shield Of Faith"]
